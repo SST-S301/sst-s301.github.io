7bbadb2673377dfdefc919d5e7d820f25d1ac7ee
Ryan Theodore The <ryan.the.2006@gmail.com>
2021-08-21 13:49:27 +0800

    Fix insertAds

--- insertAds.tsx
+++ insertAds.tsx
@@ -1,12 +1,14 @@
-const ins = document.createElement("ins");
-ins["className"] = "adsbygoogle";
-ins.setAttribute("style", "display: block")
-ins.setAttribute("data-ad-client", "ca-pub-5037854579642309")
-ins.setAttribute("data-ad-slot", "6200648204")
-ins.setAttribute("data-ad-format", "auto")
-ins.setAttribute("data-full-width-responsive", "true")
+if (typeof window !== "undefined") {
+  const ins = document.createElement("ins");
+  ins["className"] = "adsbygoogle";
+  ins.setAttribute("style", "display: block");
+  ins.setAttribute("data-ad-client", "ca-pub-5037854579642309");
+  ins.setAttribute("data-ad-slot", "6200648204");
+  ins.setAttribute("data-ad-format", "auto");
+  ins.setAttribute("data-full-width-responsive", "true");
 
-setTimeout(() => {
-  console.log("insert");
-  document.querySelector("header")?.appendChild(ins);
-}, 2000);
+  setTimeout(() => {
+    console.log("insert");
+    document.querySelector("header")?.appendChild(ins);
+  }, 2000);
+}
